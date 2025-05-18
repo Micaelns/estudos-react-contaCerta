893ad6e39452189f4bd1c6cce8b2b62a5daabae7
Micael Souza <micael.ns@hotmail.com>
2025-05-18 02:36:24 -0300

    Ajuste autenticação - contexto

--- src/components/organisms/Layout/Index.tsx
+++ src/components/organisms/Layout/Index.tsx
@@ -1,15 +1,21 @@
 import Menu from "@/components/molecules/Menu/Index";
+import Login from "@/components/organisms/Login";
+import { useAuth } from "@/data/contexts/authContext";
 
 export default function Layout(props: any) {
+  const { loggedUser } = useAuth();
+  // return loggedUser ? <Layout>Micael Souza</Layout> : <Login />;
   return (
     <div className="h-screen flex">
       <div className="w-full flex flex-col items-center">
-        <div className="bg-gray-100 w-11/12">
-          <Menu />
-        </div>
-        <div className="bg-gray-100 w-11/12 p-2 overflow-auto h-screen">
-          {props.children}
-        </div>
+        <div className="bg-gray-100 w-11/12">{loggedUser ? <Menu /> : ""}</div>
+        {loggedUser ? (
+          <div className="bg-gray-100 w-11/12 p-2 overflow-auto h-screen">
+            {props.children}
+          </div>
+        ) : (
+          <Login />
+        )}
       </div>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,5 @@
 import Layout from "@/components/organisms/Layout/Index";
-import Login from "@/components/organisms/Login";
-import { useAuth } from "@/data/contexts/authContext";
 
 export default function Home() {
-  const { loggedUser } = useAuth();
-  return loggedUser ? <Layout>Micael Souza</Layout> : <Login />;
+  return <Layout>Micael Souza</Layout>;
 }
